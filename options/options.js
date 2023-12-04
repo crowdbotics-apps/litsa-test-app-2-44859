41e543f4f6b1471c9be9ad97baa882cb032404fa
Crowdbotics <team@crowdbotics.com>
2023-12-04 20:24:50 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "litsa-test-app-2-44859",
+  url: "https://litsa-test-app-2-44859.botics.co",
+  api: "https://litsa-test-app-2-44859.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
